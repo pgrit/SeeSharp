f6c29d3459ce7fe0b23d547f6f35a91138002afc
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2021-03-12 08:36:36 +0100

    fix infinite loop on nan/inf

--- SeeSharp/Common/Atomic.cs
+++ SeeSharp/Common/Atomic.cs
@@ -4,6 +4,11 @@ namespace SeeSharp.Common {
     public static class Atomic {
         public static void AddFloat(ref float target, float value) {
             float initialValue, computedValue;
+
+            // Prevent infinite loop if a pixel value is NaN
+            if (!float.IsFinite(target))
+                return;
+
             do {
                 initialValue = target;
                 computedValue = initialValue + value;

aff48400ec363d54c960be61617cbb4da4c5a1e7
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2021-05-09 17:49:49 +0200

    remove obsolete kNN kd tree stuff

--- SeeSharp.Benchmark/Program.cs
+++ SeeSharp.Benchmark/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System.Numerics;
-using SeeSharp.Datastructs;
 
 namespace SeeSharp.Benchmark {
     class Program {
         static void Main(string[] args) {
             VectorBench.BenchComputeBasisVectors(10000000);
-            // NearestNeighborBench<NearestNeighborTree>.Benchmark_10_Nearest(1, true, new NearestNeighborTree());
         }
     }
 }

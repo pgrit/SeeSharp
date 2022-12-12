965067c445b489a935c2a1c06796af2d3ab8026b
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2022-12-12 10:53:21 +0100

    net7.0

--- SeeSharp.Benchmark/Program.cs
+++ SeeSharp.Benchmark/Program.cs
@@ -1,14 +1,15 @@
-﻿using System.Numerics;
-using System.Threading;
+﻿using System;
+using SeeSharp;
+using SeeSharp.Benchmark;
 
-namespace SeeSharp.Benchmark {
-    class Program {
-        static void Main(string[] args) {
-            GenericMaterial_Sampling.QuickTest();
-            // GenericMaterial_Sampling.BenchPerformance();
-            // GenericMaterial_Sampling.Benchmark();
+var scene = Scene.LoadFromFile("Data/Scenes/CornellBox/CornellBox.json");
+scene.FrameBuffer = new(512, 512, "");
+scene.Prepare();
+new SeeSharp.Integrators.PathTracer().Render(scene);
+Console.WriteLine(scene.FrameBuffer.RenderTimeMs);
 
-            //VectorBench.BenchComputeBasisVectors(10000000);
-        }
-    }
-}
+// GenericMaterial_Sampling.QuickTest();
+// GenericMaterial_Sampling.BenchPerformance();
+// GenericMaterial_Sampling.Benchmark();
+
+// VectorBench.BenchComputeBasisVectors(10000000);

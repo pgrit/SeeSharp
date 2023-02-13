9efde77b962c6b9924489d60d68e900faef17207
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2023-02-13 10:34:19 +0100

    refactor

--- SeeSharp/Common/Atomic.cs
+++ SeeSharp/Common/Atomic.cs
@@ -1,27 +1,28 @@
-using System.Threading;
+using System.Runtime.CompilerServices;
 
-namespace SeeSharp.Common {
+namespace SeeSharp.Common;
+
+/// <summary>
+/// Provides utility functions for atomic operations.
+/// </summary>
+public static class Atomic {
     /// <summary>
-    /// Provides utility functions for atomic operations.
+    /// Adds two floating point values in an atomic fashion, using a compare-and-swap.
+    /// Thread-safe version of: target += value;
     /// </summary>
-    public static class Atomic {
-        /// <summary>
-        /// Adds two floating point values in an atomic fashion, using a compare-and-swap.
-        /// Thread-safe version of: target += value;
-        /// </summary>
-        /// <param name="target">Destination</param>
-        /// <param name="value">Value to add</param>
-        public static void AddFloat(ref float target, float value) {
-            float initialValue, computedValue;
-            do {
-                initialValue = target;
-                computedValue = initialValue + value;
-            } while (
-                initialValue != Interlocked.CompareExchange(ref target, computedValue, initialValue)
-                // If another thread changes target to NaN in the meantime, we will be stuck forever
-                // since NaN != NaN is always true, and NaN + value is also NaN
-                && !float.IsNaN(initialValue)
-            );
-        }
+    /// <param name="target">Destination</param>
+    /// <param name="value">Value to add</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void AddFloat(ref float target, float value) {
+        float initialValue, computedValue;
+        do {
+            initialValue = target;
+            computedValue = initialValue + value;
+        } while (
+            initialValue != Interlocked.CompareExchange(ref target, computedValue, initialValue)
+            // If another thread changes target to NaN in the meantime, we will be stuck forever
+            // since NaN != NaN is always true, and NaN + value is also NaN
+            && !float.IsNaN(initialValue)
+        );
     }
 }

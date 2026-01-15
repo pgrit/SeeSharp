1fe4d31ffe7ba2d0e471e22f8e940d56a2ec873b
Pascal Grittmann <grittmann@cg.uni-saarland.de>
2026-01-15 10:05:51 +0100

    reorganize the examples

--- Examples/BlenderSync/Program.cs
+++ Examples/BlenderSync/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
 ProgressBar.Silent = true;
-SceneRegistry.AddSourceRelativeToScript("./Data");
+SceneRegistry.AddSourceRelativeToScript("../../Data/Scenes");
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();

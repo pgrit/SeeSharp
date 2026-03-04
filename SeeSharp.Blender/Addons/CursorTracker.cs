e1924cc4f4314e215fb3ecc59cfc7a331814eac6
NhatMinh2208 <minh33haha@gmail.com>
2026-03-04 15:15:13 +0100

    correct convention, remove comment, use Logger

--- SeeSharp.Blender/Addons/CursorTracker.cs
+++ SeeSharp.Blender/Addons/CursorTracker.cs
@@ -1,13 +1,10 @@
-namespace SeeSharp.Blender
-{
-    public class CursorTrackerClient 
-    {
-        public readonly CursorTrackedHandler _cursor_tracked;
-        public CursorTrackerClient(IEnumerable<IBlenderEventHandler> handlers)
-        {
-            _cursor_tracked = handlers
-                .OfType<CursorTrackedHandler>()
-                .Single(); // or First()
-        } 
-    }    
-}
+namespace SeeSharp.Blender;
+
+public class CursorTrackerClient {
+    public readonly CursorTrackedHandler _cursorTracked;
+    public CursorTrackerClient(IEnumerable<IBlenderEventHandler> handlers) {
+        _cursorTracked = handlers
+            .OfType<CursorTrackedHandler>()
+            .Single(); 
+    } 
+}    
--- SeeSharp.Blender/Addons/PathViewer.cs
+++ SeeSharp.Blender/Addons/PathViewer.cs
@@ -1,22 +1,19 @@
-namespace SeeSharp.Blender
-{
-    public class PathViewerClient 
-    {
-        public readonly CreatedHandler _created;
-        public readonly DeletedHandler _deleted;
-        public readonly SelectedHandler _selected;
+namespace SeeSharp.Blender;
 
-        public PathViewerClient(IEnumerable<IBlenderEventHandler> handlers)
-        {
-            _created = handlers
-                .OfType<CreatedHandler>()
-                .Single(); // or First()
-            _deleted = handlers
-                .OfType<DeletedHandler>()
-                .Single(); 
-            _selected = handlers
-                .OfType<SelectedHandler>()
-                .Single(); 
-        } 
-    }    
-}
+public class PathViewerClient {
+    public readonly CreatedHandler _created;
+    public readonly DeletedHandler _deleted;
+    public readonly SelectedHandler _selected;
+
+    public PathViewerClient(IEnumerable<IBlenderEventHandler> handlers) {
+        _created = handlers
+            .OfType<CreatedHandler>()
+            .Single(); 
+        _deleted = handlers
+            .OfType<DeletedHandler>()
+            .Single(); 
+        _selected = handlers
+            .OfType<SelectedHandler>()
+            .Single(); 
+    } 
+}    

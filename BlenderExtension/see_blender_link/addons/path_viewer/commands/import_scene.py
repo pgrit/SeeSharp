05a4cce4ee9ddb3fe2b57d7ae98e6facb5b98e30
NhatMinh2208 <minh33haha@gmail.com>
2026-05-14 00:48:26 +0200

    modify py-blender importer to check for .blend first

--- BlenderExtension/see_blender_link/addons/path_viewer/commands/import_scene.py
+++ BlenderExtension/see_blender_link/addons/path_viewer/commands/import_scene.py
@@ -1,7 +1,20 @@
 import bpy
+import os
 
 def handle_import_scene(msg):
     file_name = msg.get("scene_name")
+    if not file_name:
+        print("No scene path received.")
+        return
+
+    # Folder containing the received file
+    scene_folder = os.path.dirname(file_name)
+
+    # Folder name becomes the base name
+    base_name = os.path.basename(scene_folder)
+
+    blend_path = os.path.join(scene_folder, f"{base_name}.blend")
+    json_path = os.path.join(scene_folder, f"{base_name}.json")
     def run():
         if bpy.context.mode != 'OBJECT':
             bpy.ops.object.mode_set(mode='OBJECT')
@@ -25,6 +38,22 @@ def handle_import_scene(msg):
             if col != scene_collection:
                 bpy.data.collections.remove(col)
 
-        bpy.ops.import_scene.seesharp(filepath=file_name)
+        # .blend exists -> load it directly
+        if os.path.exists(blend_path):
+            print(f"Loading blend file: {blend_path}")
+
+            bpy.ops.wm.open_mainfile(filepath=blend_path)
+            return None
+
+        # only .json exists -> import and save blend
+        if os.path.exists(json_path):
+            print(f"Importing scene from json: {json_path}")
+            bpy.ops.import_scene.seesharp(filepath=json_path)
+
+            # Save blend file
+            print(f"Saving blend file: {blend_path}")
+            bpy.ops.wm.save_as_mainfile(filepath=blend_path)
+
+            return None
 
     bpy.app.timers.register(run, first_interval=0)

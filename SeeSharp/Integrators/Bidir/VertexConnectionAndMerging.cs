f80db4ea4e03ba0ab1074d25eaf49d703cb62e4d
Minh Nguyen <86256035+NhatMinh2208@users.noreply.github.com>
2026-07-22 09:18:34 +0200

    Fixing NaN errors (#36)
    * avoid dividing by zero on merge operation
    
    * allow denominator to be near zero

--- SeeSharp/Integrators/Bidir/VertexConnectionAndMerging.cs
+++ SeeSharp/Integrators/Bidir/VertexConnectionAndMerging.cs
@@ -353,13 +353,12 @@ public class VertexConnectionAndMergingBase<CameraPayloadType>
         var ancestor = PathCache[idx.pathIdx, idx.vertexIdx - 1];
         var dirToAncestor = Vector3.Normalize(ancestor.Point.Position - shader.Point.Position);
         var bsdfValue = shader.Evaluate(dirToAncestor);
-        bsdfValue *=
-            float.Abs(Vector3.Dot(shader.Point.ShadingNormal, dirToAncestor))
-            / float.Abs(Vector3.Dot(photon.Point.Normal, dirToAncestor));
+        float cosGeometry = float.Abs(Vector3.Dot(photon.Point.Normal, dirToAncestor));
+        bsdfValue *= float.Abs(Vector3.Dot(shader.Point.ShadingNormal, dirToAncestor)) / cosGeometry;    
         var photonContrib = photon.Weight * bsdfValue / NumLightPaths;
 
         // Early exit + prevent NaN / Inf
-        if (photonContrib == RgbColor.Black)
+        if (photonContrib == RgbColor.Black || cosGeometry == 0.0f)
             return RgbColor.Black;
         // Prevent outliers due to numerical issues with photons arriving almost parallel to the surface
         if (Math.Abs(Vector3.Dot(dirToAncestor, shader.Point.Normal)) < 1e-4f)
